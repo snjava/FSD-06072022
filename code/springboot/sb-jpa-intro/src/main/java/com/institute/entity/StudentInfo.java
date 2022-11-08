fd44589dc97f5018e1ca9c4f469417231f563584
snjava <61556337+snjava@users.noreply.github.com>
2022-11-08 10:35:54 +0530

    Spring Boot JPA Intro
    Spring Boot JPA Intro

---
+++ code/springboot/sb-jpa-intro/src/main/java/com/institute/entity/StudentInfo.java
@@ -0,0 +1,37 @@
+package com.institute.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class StudentInfo {
+	@Id
+	private int id;
+	private String name;
+	private String contact;
+	private String city;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+}
